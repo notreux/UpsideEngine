23752a945291756b338c99030e23990ded1aa5eb
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-04 22:16:12 +0200

    Create BaseObject.lua

---
+++ BaseObject.lua
@@ -0,0 +1,27 @@
+local self, properties, methods, data = {}, {}, {}, require(script.Parent.data);
+
+properties.name = "Base";
+properties.class = "BaseObject";
+
+properties.rotation = 0;
+properties.zIndex = 0;
+
+properties.loaded = false;
+properties.canCollide = true;
+properties.visible = true;
+
+properties.size = Vector2.new();
+properties.position = Vector2.new();
+properties.hitbox = Vector2.new();
+
+self.index = math.random();
+
+self.functions = {};
+self.properties = properties;
+self.methods = methods;
+self.base = true;
+
+self.inherited = { properties.class };
+data.space[self.index] = self;
+
+return setmetatable({}, data.getMetaData(self));
