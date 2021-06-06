900fd88eccf0ad2ed3f953711012e9f6ed6d31d7
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-06 02:13:58 +0200

    Update internal.lua

--- src/internal.lua
+++ src/internal.lua
@@ -1,49 +1,98 @@
-local internal, data = setmetatable({}, {}), require(script.Parent.data);
+local internal, data = setmetatable({}, {}), require(script.Parent:WaitForChild("data"));
+
+function getAxis(c1, c2)
+
+	local axis = {};
+
+	axis[1] = (c1[2] - c1[1]).Unit;
+	axis[2] = (c1[4] - c1[1]).Unit;
+	axis[3] = (c2[2] - c2[1]).Unit;
+	axis[4] = (c2[4] - c2[1]).Unit;
+
+	return axis;
+
+end
+
+function getCorners(object)
+
+	local corners, rot, ap, as = {}, math.rad(object.Rotation),
+		  object.AbsolutePosition, object.AbsoluteSize;
+
+	local center, wc = object.AbsolutePosition + object.absoluteSize/2, {
+
+		Vector2.new(ap.X, ap.Y);
+		Vector2.new(ap.X + as.X, ap.Y);
+		Vector2.new(ap.X + as.X, ap.Y + as.Y);
+		Vector2.new(ap.X, ap.Y + as.Y);
+
+	};
+
+	for i, corner in ipairs(wc) do
+
+		corners[i] = Vector2.new(
+
+			center.X + (corner.X - center.X) * math.cos(rot) - (corner.Y - center.Y) * math.sin(rot),
+			center.Y + (corner.X - center.X) * math.sin(rot) + (corner.Y - center.Y) * math.cos(rot)
+
+		);
+
+	end
+
+	return corners;
+
+end
 
 function internal.updateSpace(this)
-	
+
 	for _, meta in pairs(data.space) do
-		
+
 		if meta.canCollide and meta.loaded and this.index ~= meta.index then
-			
-			local cp, cs, mp, ms = this.properties.position,
-			
-				  this.properties.hitbox.X > 0 and this.properties.hitbox or this.properties.size,
-			
-				 	 meta.properties.position, 
-			
-						  meta.properties.hitbox.X > 0 and this.properties.hitbox or meta.properties.size;
-	
-			if cp.X < (mp.X + ms.X) 
-				
-				and(cp.X + cs.X) > mp.X
-				
-					and cp.Y < (mp.Y + ms.Y) 
-				
-						and(cs.Y + cp.Y) > mp.Y 
-				
-							and not this.collisions[meta.index] then
-				
-								this.fire("collision", meta);
-									meta.fire("collision", this);
-								
-										meta.collisions[this.index] = this;
-											this.collisions[meta.index] = meta;
 
-							elseif this.collisions[meta.index] then
-				
-								this.fire("collisionEnded", meta);
-									meta.fire("collisionEnded", this);
+			local c1, c2, collision = getCorners(this), getCorners(meta), nil;
+			local axis = getAxis(c1, c2);
 
-										meta.collisions[this.index] = nil;
-											this.collisions[meta.index] = nil;
-					
-						end
-			
+			for i = 1, #axis do
+
+				local s1, s2, ax = {}, {}, axis[i]:Dot();
+
+				for n = 1, 4 do
+
+					table.insert(s1, ax, c1[n]);
+					table.insert(s2, ax, c2[n]);
+
 				end
-	
+
+				local s1max, s1min = math.max(unpack(s1)), math.min(unpack(s1));
+				local s2max, s2min = math.max(unpack(s2)), math.min(unpack(s2));
+
+				if s2min > s1max or s2max < s1min then collision = false; break; end;
+
+			end
+			
+			collision = collision == false and false or true
+			
+			if collision and not this.collisions[meta.index] then
+
+				this.fire("collision", meta);
+				meta.fire("collision", this);
+
+				meta.collisions[this.index] = this;
+				this.collisions[meta.index] = meta;
+
+			elseif not collision and this.collisions[meta.index] then
+
+				this.fire("collisionEnded", meta);
+				meta.fire("collisionEnded", this);
+
+				meta.collisions[this.index] = nil;
+				this.collisions[meta.index] = nil;
+
+			end
+
 		end
-	
+
+	end
+
 end
 
 return internal;
