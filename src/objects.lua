ec90524e2df15af51bcb1d8ec2df4747bc274d2c
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-06 12:22:21 +0200

    Update objects.lua

--- src/objects.lua
+++ src/objects.lua
@@ -1,20 +1,22 @@
 local self, base = {}, require(script.Parent:WaitForChild("BaseObject"));
 
 return function(obj)
-		
+
+	print(obj)
+
 	if obj == "static" then
-		print("static passed")
+		
 		base.properties.image = "";
 		base("basics", "static");
-		
+
 	elseif obj == "sprite" then
-		print("sprite passed")
+		
 		base({"basics", "layers"}, "sprite");
-		
+
 	elseif obj == "particles" then
-		
+
 		if obj then return error("For now, u can't use the particles object, instead use sprite to make particles"); end;
-		
+
 		base.properties.texture = "";
 		base.properties.canCollide = false;
 		base.properties.speed = 0;
@@ -25,28 +27,28 @@ return function(obj)
 		base.properties.rate = 10;
 		base.properties.enabled = true;
 		base.properties.rotationSpeed = 0;
-		
+
 		base({"basics", "layers"}, "particles");
-		
+
 	elseif obj == "light" then
-		print("light passed")
+		
 		base.properties.transparency = 0.5;
 		base.properties.range = 5;
 		base.properties.image = "rbxassetid://6904405933";
 		base.properties.zIndex = 999999999;
-		
+
 		base({"basics", "light"}, "light");
 
 	else
-		
+
 		return error("Invalid object!");
-		
+
 	end
-	
+
 	base.properties.size = Vector2.new(0.2,0.2);
-	
+
 	base.base = nil;
-	
+
 	return base;
-	
+
 end;
