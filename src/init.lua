6b667b755701e06636576921ce42b2cfe9c590eb
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-06 02:12:08 +0200

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -8,15 +8,21 @@ end
 
 function self.start(renderIn)
 	
-	ambient = Instance.new("Frame");
-	ambient.BorderSizePixel = 0;
-	ambient.Parent = renderIn;
-	ambient.AnchorPoint = Vector2.new(0.5, 0.5);
-	ambient.Position = UDim2.new(0.5, 0.5);
-	ambient.BackgroundTransparency = 0.8;
-	ambient.BackgroundColor3 = Color3.fromRGB(255,255,255);
-	ambient.ZIndex = 999999998;
-	
+	if not renderIn:FindFirstChild("___________Ambient") then
+		
+		ambient = Instance.new("Frame");
+		ambient.BorderSizePixel = 0;
+		ambient.Parent = renderIn;
+		ambient.AnchorPoint = Vector2.new(0.5, 0.5);
+		ambient.Position = UDim2.new(0.5, 0, 0.5, 0);
+		ambient.Size = UDim2.new(1,0,1,0)
+		ambient.BackgroundTransparency = 0.8;
+		ambient.BackgroundColor3 = Color3.fromRGB(255,255,255);
+		ambient.ZIndex = 999999998;
+		ambient.Name = "___________Ambient";
+		
+	end
+		
 	for index, val in pairs(indexs) do
 		
 		pcall(function()
@@ -27,12 +33,12 @@ function self.start(renderIn)
 		
 	end
 	
-	local storage = game.ServerStorage:FindFirstChild("Upside Engine Storage")
-		            or game.ReplicatedStorage:FindFirstChild("Upside Engine Storage");
+	local storage = game.ServerStorage:FindFirstChild("UpsideEngineDataBase")
+					or game.ReplicatedStorage:FindFirstChild("UpsideEngineDataBase");
 	
 	if storage then
 		
-		local rebuilded = self.rebuildSpace(HTTPS:JSONDecode(storage and storage:IsA("ModuleScript") and require(storage) or {}));
+		local rebuilded = self.rebuildSpace(HTTPS:JSONDecode(storage and string.len(storage.Value) > 1 and storage.Value or {}));
 		
 		for index, value in pairs(rebuilded) do
 			
@@ -42,12 +48,12 @@ function self.start(renderIn)
 		
 	end
 		
-	table.foreach(data.workspace, function(obj)
+	for _, obj in pairs(data.workspace) do
 		
 		obj.build(renderIn);
 		
-	end)
-		
+	end
+	
 end
 
 function self.rebuildSpace(wkspace)
