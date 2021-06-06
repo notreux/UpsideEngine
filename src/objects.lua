2bdff07953a58a2a5a6714e6f890e57b887e1b73
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-06 02:15:28 +0200

    Update objects.lua

--- src/objects.lua
+++ src/objects.lua
@@ -1,39 +1,41 @@
-local self, base = {}, require(script.Parent.Parent.BaseObject);
+local self, base = {}, require(script.Parent:WaitForChild("BaseObject"));
 
 return function(obj)
 	
+	print(obj)
+	
 	if obj == "static" then
-		
-		base.image = "";
+		print("static passed")
+		base.properties.image = "";
 		base("basics", "static");
 		
 	elseif obj == "sprite" then
-		
+		print("sprite passed")
 		base({"basics", "layers"}, "sprite");
 		
 	elseif obj == "particles" then
 		
-		if obj == nil then return error("For now, u can't use the particles object, instead use sprite to make particles"); end;
+		if obj then return error("For now, u can't use the particles object, instead use sprite to make particles"); end;
 		
-		base.texture = "";
-		base.canCollide = nil;
-		base.speed = 0;
-		base.angle = Vector2.new();
-		base.gravity = 0;
-		base.transparency = 0;
-		base.lightEmission = 0;
-		base.rate = 10;
-		base.enabled = true;
-		base.rotationSpeed = 0;
+		base.properties.texture = "";
+		base.properties.canCollide = false;
+		base.properties.speed = 0;
+		base.properties.angle = Vector2.new();
+		base.properties.gravity = 0;
+		base.properties.transparency = 0;
+		base.properties.lightEmission = 0;
+		base.properties.rate = 10;
+		base.properties.enabled = true;
+		base.properties.rotationSpeed = 0;
 		
 		base({"basics", "layers"}, "particles");
 		
 	elseif obj == "light" then
-		
-		base.opacity = 0.5;
-		base.range = 5;
-		base.image = "rbxassetid://6904405933";
-		base.zIndex = 999999999;
+		print("light passed")
+		base.properties.transparency = 0.5;
+		base.properties.range = 5;
+		base.properties.image = "rbxassetid://6904405933";
+		base.properties.zIndex = 999999999;
 		
 		base({"basics", "light"}, "light");
 
@@ -42,6 +44,8 @@ return function(obj)
 		return error("Invalid object!");
 		
 	end
+	
+	base.properties.size = Vector2.new(0.2,0.2);
 	
 	base.base = nil;
 	
