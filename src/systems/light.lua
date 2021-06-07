8ec9919be8db371495011d10101bec4ff8625ed1
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-07 21:18:57 +0200

    Update light.lua

--- src/systems/light.lua
+++ src/systems/light.lua
@@ -1,29 +1,29 @@
 local data = require(script.Parent.Parent:WaitForChild("data"));
 
-return function(this)
-	
+return {setup = function(this)
+
 	this.on("changed", function(property)
-		
+
 		if property == "range" then
-			
+
 			this.size = UDim2.new(this.range / 100, 0, this.range / 100, 0);
-			
+
 			data.robloxSpace[this.index].Size = this.size;
-			
+
 		elseif property == "zIndex" and this.zIndex ~= 999999999 then
-			
+
 			this.zIndex = 999999999;
-			
+
 		elseif property == "size" and this.size.X ~= this.range/100 then
-			
+
 			this.size = Vector2.new(this.range / 100, this.range / 100)
-			
+
 		end
-		
+
 	end)
-	
+
 	this.on("loaded"):wait();
 
 	Instance.new("UIAspectRatioConstraint").Parent = data.robloxSpace[this.index];
-	
-end
+
+end}
