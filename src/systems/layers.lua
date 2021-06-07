b998d0de164153a2647ab42b4f1b47a8b73c8c17
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-07 21:10:59 +0200

    Update layers.lua

--- src/systems/layers.lua
+++ src/systems/layers.lua
@@ -1,80 +1,75 @@
-local data, contentProvider, rs = require(script.Parent.Parent:WaitForChild("data")), game:GetService("ContentProvider"), game:GetService("RunService");
+local data, contentProvider, rs, __ = require(script.Parent.Parent:WaitForChild("data")), game:GetService("ContentProvider"), game:GetService("RunService"), {};
 
-return function(this)
-	
-	this.properties.spriteSpeed = 0.05;
-	this.layers = {};
-	this.actions = {};
-	
-	function this.methods.setLayer(name, layers, rectSize, frames)
-		
-		assert(typeof(name) == "string", "You must define a valid name!");
-		
-		contentProvider:PreloadAsync(typeof(layers) == "table" and layers or {layers});
 
-		this.layers[name] = {
-			
-			layers = layers;
-			frames = frames;
-			rectSize = rectSize or Vector2.new();
-			
-		};
-		
-		this.actions[name] = false;
+function __:setLayer(self, name, layers, rectSize, frames)
+
+	assert(typeof(name) == "string", "You must define a valid name!");
+
+	contentProvider:PreloadAsync(typeof(layers) == "table" and layers or {layers});
+
+	self.layers[name] = {
+
+		layers = layers;
+		frames = frames;
+		rectSize = rectSize or Vector2.new();
+
+	};
+
+	self.actions[name] = false;
+
+end
+
+function __.play(self, name)
+
+	self.playing = false;
+
+	if not self.loaded then self.on('loaded'):wait(); end
+
+	self.methods:fire("layerStarted", name, unpack(self.layers[name]))
+
+	self.playing = true;
+
+	local object, cn = data.robloxSpace[self.index], nil;		
+
+	object.ImageRectSize = self.layers[name].rectSize;
+
+	local function everySprite(layer)
+
+		object.Image = layer;
+
+		wait(self.properties.spriteSpeed)
 
 	end
-	
-	function this.methods.play(name)
-		
-		this.playing = false;
-		
-		if not this.loaded then this.on('loaded'):wait(); end
-		
-		this.fire("layerStarted", name, unpack(this.layers[name]))
-		
-		this.playing = true;
 
-		local object, cn = data.robloxSpace[this.index], nil;		
-		
-		object.ImageRectSize = this.layers[name].rectSize;
-		
-		local function everySprite(layer)
+	local function everyFrame(frame)
 
-			object.Image = layer;
+		object.ImageRectOffset = frame * object.ImageRectSize;
 
-			wait(this.spriteSpeed)
+		wait(self.properties.spriteSpeed);
+
+	end		
+
+	cn = rs.Heartbeat:Connect(function()
+
+		if self.playing then
+
+			table.foreach(self.layers[name].frames or self.layers, self.layers[name].frames and everyFrame or everySprite)
+
+		else cn:Disconnect();
 
 		end
-		
-		local function everyFrame(frame)
 
-			object.ImageRectOffset = frame * object.ImageRectSize;
+	end)
 
-			wait(this.spriteSpeed);
-
-		end		
-		
-		cn = rs.Heartbeat:Connect(function()
-			
-			if this.playing then
-								
-				table.foreach(this.layers[name].frames or this.layers, this.layers[name].frames and everyFrame or everySprite)
-				
-			else cn:Disconnect();
-				
-			end
-				
-		end)
-			
-	end
-	
-	function this.methods.stop(name)
-		
-		this.fire("layerStarted", name, unpack(this.layers[name]));
-		this.playing = false;
-		this.actions[name] = false;
-
-	end
-	
-	
 end
+
+function __.stop(self, name)
+
+	self.methods:fire("layerStarted", name, unpack(self.layers[name]));
+	self.playing = false;
+	self.actions[name] = false;
+
+end
+
+
+return __;
