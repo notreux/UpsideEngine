eb82fc8e6803f2fba1c7f6122dda8818c1916b60
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-07 21:12:51 +0200

    Update init.lua

--- src/systems/init.lua
+++ src/systems/init.lua
@@ -1,17 +1,47 @@
-local self = {}
+local self, exclusivePermissions = {}, {
+
+	"play",
+	"stop",
+	"build"	
+
+};
 
 for _, n in pairs(script:GetChildren()) do
-	
+
 	self[n.Name] = require(n)
-	
+
 end
 
-return function(object, methods)
-	
-	for _, method in pairs(methods) do
-		
-		self[method](object);
-		
+return function(object, directAcces, systems)
+
+	for _, system in pairs(systems) do
+
+		local system = self[system];
+
+		for index, method in pairs(system) do
+
+			if table.find(exclusivePermissions, method) then
+
+				object[system] = function(...)
+					
+					local params = {...};
+
+					assert(params[1] and params[1].index and params[1].index == object.index, "Expected ':' not '.' calling member method " .. method);
+					
+					table.remove(params, 1);
+					
+					self[system](directAcces, params);
+					
+				end
+					
+			else
+				
+				object[system] = self[system];
+				
+			end 
+
+		end
+
 	end
-	
+
 end;
