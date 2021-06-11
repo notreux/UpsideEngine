78372032670a4a1a3661a7fe411b3ee54527f78a
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-11 18:13:45 +0200

    Update basics.lua

--- src/systems/basics.lua
+++ src/systems/basics.lua
@@ -1,163 +1,208 @@
 local rs, data, __ = game:GetService("RunService"), require(script.Parent.Parent:WaitForChild("data")), {};
 
-	function __:on(self, event_name, __function)
-		
-		if not self.events[event_name] then
+function __.on(this, event_name, __function)
+
+	if not this.events[event_name] then
+
+		this.events[event_name] = {}
+
+	end
+
+	local index = #this.events[event_name] + 1;
+
+	this.events[event_name].waiting = not this.events[event_name].waiting == "///waiting///" or this.events[event_name].waiting;
+
+	this.events[event_name][index] = __function;
+
+	return {
+
+		wait = function()
+
+			while this.events[event_name].waiting == "///waiting///" and wait() do end
 			
-			self.events[event_name] = {}
-			
+			local ew = this.events[event_name].waiting;
+
+			this.events[event_name].waiting = "///waiting///";
+
+			return ew;
+
+		end,
+
+		disconnect = function()
+
+			table.remove(this.events[event_name], index);
+
 		end
-		
-		local index = #self.events[event_name] + 1;
 
-		self.events[event_name].waiting = "///waiting///";
-		
-		self.events[event_name][index] = __function;
+	}
 
-		return {
-			
-			wait = function()
-				
-				while self.events[event_name].waiting == "///waiting///" and wait() do end
-				
-				local ew = self.events[event_name].waiting;
-				
-				self.events[event_name].waiting = "///waiting///";
-				
-				return ew;
-				
-			end,
-			
-			disconnect = function()
-				
-				table.remove(self.events[event_name], index);
-				
-			end
-			
-		}
-		
+end
+
+function __.fire(self, name, ...)
+
+	if not self.events[name] then
+
+		self.events[name] = {}
+
 	end
-	
-	function __:fire(self, name, ...)
-		
-		self.events[name].waiting = ...;	
-		
-		for _, f in pairs(self.events[name] or {}) do
-				
-			f(...);
-				
+
+	self.events[name].waiting = ...;	
+
+	for index, f in pairs(self.events[name]) do
+
+		if index ~= "waiting" then
+
+			coroutine.wrap(f)(...);
+
 		end
-		
+
 	end
 
-	function __:addTag(self, Tag)
+end
+
+function __.addTag(self, Tag)
+
+	self.tags[Tag] = true;
+
+end
+
+function __.hasTag(self, Tag)
+
+	return self.tags[Tag];
+
+end
+
+function __.removeTag(self, Tag)
+
+	self.tags[Tag] = nil;
+
+end
+
+function __.tween(self, p, t, v)
+
+	local init, vel, cn = self[p], self[p] - v/t, nil;
+
+	cn = rs.Heartbeat:Connect(function()
+
+		if (init > v == v < self[p] or init < v == v > self[p]) and wait(vel) then
+
+			self[p] += vel;
+
+		else
+
+			cn:Disconnect();
+
+		end
+
+	end)
+
+end
+
+function __.checkCollision(self, meta)
+
+	assert(meta and meta.inherited and meta.inherited[1] == "BaseObject", "Invalid object")
+
+	return self.collisions[meta.index] and true or false;
+
+end
+
+function __.destroy(self) 
+
+	self:fire("destroyed", os.clock());
+
+	if data.robloxSpace[self.index] then
+
+		data.robloxSpace[self.index]:Destroy();
+
+		data.space[self.index] = nil;
+		data.workspace[self.name] = nil;
+
+	end	
+
+end
+
+function __.extend(self, class, props)
+	
+	if not self.loaded then self:on('loaded'):wait(); end;
+	
+	local metaCopy =  { properties = {}, functions = {}, base = true };
+
+	for index, val in pairs(self.properties) do
 		
-		self.tags[Tag] = true;
+		metaCopy.properties[index] = val;
+
+	end
+	
+	for index, val in pairs(self.functions) do
+
+		metaCopy.functions[index] = val;
+
+	end
+	
+	metaCopy.inherited = self.inherited;
+	
+	metaCopy.events = {};
+	metaCopy.collisions = {};
+	
+	metaCopy.events = {};
+	metaCopy.tags = {};
+	
+	metaCopy.index = math.random();
+
+	local clon = setmetatable({}, data.getMetaData(metaCopy)); 
+	clon(self.modules, class)
+	clon.base = false;
+	clon.name = self.name ~= clon.name and clon.name or clon.name .. tostring(math.random());
+	
+	data.space[clon.index] = clon;
+	data.workspace[clon.name] = clon;
+	for index, value in pairs(props) do
+		
+		clon[index] = value;
 		
 	end
 	
-	function __:hasTag(self, Tag)
+	return clon;
 
-		return self.tags[Tag];
+end
 
-	end
+function __.build(self, renderIn)
+
+	renderIn = renderIn or game.StarterGui:FindFirstChildOfClass("ScreenGui") or game.StarterGui;
+
+	local object = Instance.new("ImageLabel");
+	object.Parent = renderIn;
+	object.AnchorPoint = Vector2.new(0.5,0.5);
+	object.BackgroundTransparency = 1;
+	object.Image = self.properties.image or "";
 	
-	function __:removeTag(self, Tag)
-
-		self.tags[Tag] = nil;
-
-	end
+	data.robloxSpace[self.index] = object;
 	
-	function __:tween(self, p, t, v)
-		
-		local init, vel, cn = self[p], self[p] - v/t, nil;
-		
-		cn = rs.Heartbeat:Connect(function()
-			
-			if (init > v == v < self[p] or init < v == v > self[p]) and wait(vel) then
+	self.loaded = true;
+	self:fire("loaded", os.clock());
+	
+	for index, value in pairs(self.properties) do
 
-				self[p] += vel;
-				
-			else
-				
-				cn:Disconnect();
-				
-			end
-			
+		pcall(function()
+
+			data.space[self.index][index] = value;
+
 		end)
-				
-	end
-	
-	function __:checkCollision(self, meta)
-		
-		assert(meta and meta.inherited and meta.inherited[1] == "BaseObject", "Invalid object")
-		
-		return self.collisions[meta.index] and true or false;
-				
-	end
-	
-	function __:destroy() 
-		
-		self.fire("destroy");
-		
-		if data.robloxSpace[self.index] then
-			
-			data.robloxSpace[self.index]:Destroy();
-			
-			data.space[self.index] = nil;
-			data.workspace[self.name] = nil;
-			
-		end	
-		
-	end
-	
-	function __:extend(self, class)
 
-		local copy, metaCopy = data.robloxSpace[self.index]:Clone(), {};
-		copy.Parent = data.robloxSpace[self.index].Parent;
-		
-		for index, val in pairs(self) do
-			
-			metaCopy[index] = val;
-			
-		end
-		
-		metaCopy.index = math.random();
-		data.robloxSpace[metaCopy.index] = copy;
-		
-		local clon = setmetatable({}, data.getMetaData(metaCopy))(self.modules, class); 
-		
-		data.space[metaCopy.index] = clon;
-		data.workspace[metaCopy.name] = clon;
-		
-		return clon;
-		
 	end
-	
-	function __.build(self, renderIn)
+
+end
+
+function __.applySymmetry(this)
+
+	spawn(function()
 		
-		renderIn = renderIn or game.StarterGui:FindFirstChildOfClass("ScreenGui") or game.StarterGui;
+		if not this.loaded then this:on("loaded"):wait(); end;
 		
-		local object = Instance.new("ImageLabel");
-		object.Parent = renderIn;
-		object.AnchorPoint = Vector2.new(0.5,0.5);
-		
-		for index, value in pairs(self.properties) do
-			
-			pcall(function()
+		Instance.new("UIAspectRatioConstraint").Parent = data.robloxSpace[this.index];
 				
-				self.properties[index] = value;
-				
-			end)
-			
-		end
-		
-		data.robloxSpace[self.index] = object
-	
-		self:fire("loaded", os.clock());
-	
-		self.loaded = true;
-		
-	end
-	
+	end)
+
+end
+
 return __;
