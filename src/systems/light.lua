79e1b3c354c987e6d475ef8bdd60cc0651da114f
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-13 15:32:15 +0200

    Update light.lua

--- src/systems/light.lua
+++ src/systems/light.lua
@@ -2,25 +2,25 @@ local data = require(script.Parent.Parent:WaitForChild("data"));
 
 return {
 
-	setup = function(self)
-		
+	fast = { setup = function(self)
+
 		self:applySymmetry();
 
 		self:on("changed", function(property)
 
 			if property == "range" then
-									
+
 				wait()
 				self.size = Vector2.new(self.range / 100, self.range / 100);
-				
+
 			elseif property:lower() == "zIndex" and self.zIndex ~= 999999999 then
-				
+
 				self.zIndex = 999999999;
-				
+
 			end
 
 		end)
-		
-	end
+
+	end }
 
 }
