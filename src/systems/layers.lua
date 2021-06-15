8d7b20c1a646cd18470d39e9c3cec97add18aa70
NotReux <66805474+NotReux@users.noreply.github.com>
2021-06-15 02:04:30 +0200

    Update layers.lua

--- src/systems/layers.lua
+++ src/systems/layers.lua
@@ -1,7 +1,7 @@
 local data, contentProvider, rs, __ = require(script.Parent.Parent:WaitForChild("data")), game:GetService("ContentProvider"), game:GetService("RunService"), { direct = {} };
 
 function __.direct.setLayer(self, name, layers, rectSize, frames)
-	
+
 	assert(typeof(name) == "string", "You must define a valid name!");
 
 	contentProvider:PreloadAsync(typeof(layers) == "table" and layers or {layers});
@@ -15,16 +15,16 @@ function __.direct.setLayer(self, name, layers, rectSize, frames)
 	};
 
 	self.actions[name] = false;
-		
+
 end
 
 function __.direct.play(self, name, speed)
-		
+
 
 	if not self.loaded then self:on('loaded'):wait(); end
-	
+
 	if self.playing then self:stop(); end;
-		
+
 	self:fire("layerStarted", name, self.layers[name] and unpack(self.layers[name]) or nil);
 
 	self.playing = true;
@@ -45,31 +45,27 @@ function __.direct.play(self, name, speed)
 	local function everyFrame(frame)
 
 		object.ImageRectOffset = frame * self.layers[name].rectSize;
-		
+
 		wait(speed or self.properties.spriteSpeed);
 
 	end
-	
+
 	local f = (self.layers[name].frames and everyFrame or everySprite);
-	
+
 	self.heartbeat = rs.Heartbeat:Connect(function()
 
 		if ended then
-			
-			self:fire('layerStarted', os.clock())
-			
+
 			ended = false;
-			
+
 			for _, frame in pairs(self.layers[name].frames or self.layers[name]) do
-				
+
 				f(frame)
-				
+
 			end
-			
+
 			ended = true;
-			
-			self:fire('layerEnded', os.clock())
-			
+
 		end
 
 	end)
@@ -77,27 +73,27 @@ function __.direct.play(self, name, speed)
 end
 
 function __.direct.stop(self, name)
-	
+
 	self.playing = false;
-	
+
 	if not name then
-	
+
 		for index, value in pairs(self.actions) do
-		
+
 			self.actions[index] = false;
-		
+
 		end
-		
+
 	else
-		
+
 		self.actions[name] = false;
-		
+
 	end
-	
+
 	self:fire("layerEnded", name, unpack(self.layers[name] or {}));
-	
+
 	return self.heartbeat and self.heartbeat:Disconnect() or nil;
-		
+
 end
 
 
