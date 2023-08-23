9c0fe364680cbffd2906452751bbc3251699b13e
TheHackerPuppy <ivancicoelterrible@gmail.com>
2023-08-23 22:41:10 +0200

    feature: lighting system work in parallel

--- src/Runtime/EveryStep/LightingTracker.lua
+++ src/Runtime/EveryStep/LightingTracker.lua
@@ -26,71 +26,110 @@ local function getDistance(v1, v2)
 end
 
 local function isInsideBigPixel(bpx, light, range)
-	return bpx.AbsolutePosition.X < light.absolutePosition.X + range
-		and bpx.AbsolutePosition.X + bpx.AbsoluteSize.X > light.absolutePosition.X - range
-		and bpx.AbsolutePosition.Y < light.absolutePosition.Y + range
-		and bpx.AbsolutePosition.Y + bpx.AbsoluteSize.Y > light.absolutePosition.Y - range
+	return bpx.position.X < light.Position.X + range
+		and bpx.position.X + bpx.size.X > light.Position.X - range
+		and bpx.position.Y < light.Position.Y + range
+		and bpx.position.Y + bpx.size.Y > light.Position.Y - range
 end
 
-return function(scene)
-	local current = clock()
-	local lighting = scene.LightingEnvironment
-	if current - ms < lighting.UpdateFrequency or not lighting.ready then
-		return
+local self = {}
+function self.onSetData(data, name, value)
+	if name ~= "lights" or not value then
+		return value
 	end
 
-	local resolution = scene.Instance.GameFrame.AbsoluteSize
-	local pixels = lighting.__Pixels
-
-	local lights = {}
-	local update = {}
-	ms = current
-
-	for _, light in lighting.Content do
+	for index, light in value do
 		local instance = light.Instance
 		local range = light.Range * light.Range
 
 		local absoluteSize = Vector2.new(range, range)
-		local absolutePosition = instance.AbsolutePosition + (absoluteSize / resolution)
+		local absolutePosition = instance.AbsolutePosition + (absoluteSize / data.resolution)
 
-		if not light.Track or isOutSceen(absolutePosition, absoluteSize * 2) then
+		if isOutSceen(absolutePosition, absoluteSize * 2) then
 			continue
 		end
 
-		table.insert(lights, {
-			brightness = light.Brightness,
-			absolutePosition = absolutePosition,
-			absoluteRange = range,
-			range = light.Range,
-			color = light.Color,
-			transparency = light.Transparency,
-		})
+		value[index] = {
+			Instance = light.Instance,
+			Brightness = light.Brightness,
+			Position = absolutePosition,
+			AbsoluteRange = range,
+			Range = light.Range,
+			Color = light.Color,
+			Transparency = light.Transparency,
+		}
 	end
 
-	table.sort(lights, sortf)
-	for bigPixel, childs in pixels do
+	table.sort(value, sortf)
+	return value
+end
+
+function self.build(data)
+	local details = data.details
+	local bigPixel = details.bigPixel
+	local bigPixelSize = details.bigPixelSize
+
+	for y = 1, details.screenSize.Y / bigPixelSize do
+		local px = bigPixel:Clone()
+		px.Position = UDim2.fromOffset(details.x * bigPixelSize, y * bigPixelSize)
+		px.Parent = details.screen
+
+		local childs = px.Canvas:GetChildren()
+		for i = 1, #childs do
+			local instance = childs[i]
+			childs[i] = {
+				instance = instance,
+				position = instance.AbsolutePosition,
+				size = instance.AbsoluteSize,
+			}
+		end
+
+		table.insert(data.pixels, {
+			instance = px,
+			position = px.AbsolutePosition,
+			size = px.AbsoluteSize,
+			childs = childs,
+		})
+	end
+end
+
+function self.draw(data)
+	local current = clock()
+	local lighting = data.lighting
+
+	if current - ms < lighting.updateFrequency then
+		return
+	end
+
+	local pixels = data.pixels
+	ms = current
+
+	local lights = data.lights
+	local update = {}
+
+	for bigPixel, bpxData in pixels do
 		for _, light in lights do
-			if not isInsideBigPixel(bigPixel, light, light.range) then
+			if not isInsideBigPixel(bpxData, light, light.Range) then
 				continue
-			elseif not update[bigPixel] then
-				update[bigPixel] = {}
+			elseif not update[bpxData] then
+				update[bpxData] = {}
 			end
 
-			for _, px in childs do
-				local distance = getDistance(px.position, light.absolutePosition)
-				if distance < light.absoluteRange then
-					local nDistance = normalize(distance, 0, light.absoluteRange)
-					local transparency = (px.transparency or lighting.AmbientTransparency)
+			for _, px in bpxData.childs do
+				local distance = getDistance(px.position, light.Position)
+				if distance < light.AbsoluteRange then
+					local nDistance = normalize(distance, 0, light.AbsoluteRange)
+					local transparency = (px.transparency or lighting.ambientTransparency)
 						- nDistance
-						+ light.brightness
+						+ light.Brightness
 						+ 1
 
-					local lightTransparency = (light.transparency + (px.lightTransparency or light.transparency)) * 0.5
+					local lightTransparency = (light.Transparency + (px.lightTransparency or light.Transparency)) * 0.5
 					local color = if px.color
-						then interpolateColor(px.color, light.color, 1 - nDistance)
-						else light.color
+						then interpolateColor(px.color, light.Color, 1 - nDistance)
+						else light.Color
 
-					update[bigPixel][px] = true
+					update[bpxData][px] = true
 					px.lightTransparency = lightTransparency
 					px.transparency = transparency
 					px.color = color
@@ -99,26 +138,27 @@ return function(scene)
 		end
 	end
 
+	task.synchronize()
 	for bigPixel, drawn in update do
-		local childs = pixels[bigPixel]
-		bigPixel.BackgroundTransparency = 1
-		bigPixel.Canvas.Visible = true
+		local bpxInstance = bigPixel.instance
+		bpxInstance.BackgroundTransparency = 1
+		bpxInstance.Canvas.Visible = true
 
-		for _, px in childs do
+		for _, px in bigPixel.childs do
 			local isDrawn = drawn[px]
 			local instance = px.instance
 
-			local transparency = if isDrawn and px.lightTransparency > lighting.AmbientTransparency
-				then math.clamp(px.transparency, lighting.AmbientTransparency, px.lightTransparency)
-				else lighting.AmbientTransparency
+			local transparency = if isDrawn and px.lightTransparency > lighting.ambientTransparency
+				then math.clamp(px.transparency, lighting.ambientTransparency, px.lightTransparency)
+				else lighting.ambientTransparency
 
 			local color = if isDrawn
 				then interpolateColor(
 					px.color,
-					lighting.AmbientColor,
-					math.clamp(lighting.LightIntensity - px.transparency, 0, lighting.LightIntensity)
+					lighting.ambientColor,
+					math.clamp(lighting.lightIntensity - px.transparency, 0, lighting.lightIntensity)
 				)
-				else lighting.AmbientColor
+				else lighting.ambientColor
 
 			px.color = nil
 			px.transparency = nil
@@ -128,15 +168,18 @@ return function(scene)
 		end
 	end
 
-	for bigPixel, childs in lighting.__UpdateMemory do
+	for bigPixel, childs in lighting.updateMemory do
 		if update[bigPixel] then
 			continue
 		end
 
-		bigPixel.Canvas.Visible = false
-		bigPixel.BackgroundTransparency = lighting.AmbientTransparency
-		bigPixel.BackgroundColor3 = lighting.AmbientColor
+		local instance = bigPixel.instance
+		instance.Canvas.Visible = false
+		instance.BackgroundTransparency = lighting.ambientTransparency
+		instance.BackgroundColor3 = lighting.ambientColor
 	end
 
-	lighting.__UpdateMemory = update
+	lighting.updateMemory = update
 end
+
+return self
